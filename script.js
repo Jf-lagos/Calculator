9b0c55841ef7b5b2582fcd70247d757d66b91568
Jeff <lagosjf@gmail.com>
2021-08-18 19:57:22 +0200

    design of the calculator

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+//Math function 
+
+function operate () {
+    add = (a, b) => a + b 
+    substract = (a, b) => a - b 
+    multiply = (a, b) => a * b
+    divide = (a, b) => a / b
+   
+}
+
+/* 
+Comment effectuer un calcul genre 10 + 20 ? 
+
+- Quand j'appuie une touche, affiche la touche a l'écran.
+    - Lier ces touches aux inputs des functions
+    
+- Quand je clique sur Equal montre le resultat. 
+    - Prendre les deux inputs.
+    - Les calculer (faire des functions x/-/+/).
+    - Retourner le resultat à l'écran.
+
+- Quand je clique sur ESC, effacer. 
+*/
