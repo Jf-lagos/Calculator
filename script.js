d22535fe5ff35a2af87c70e9403a89e395a58482
Jeff <lagosjf@gmail.com>
2021-08-21 18:26:43 +0200

    event listener

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 
+let buttons = document.querySelectorAll("button");
+const length = buttons.length;
+for (let i = 0; i < length; i++) {
+    buttons[i].addEventListener("click", handle);
+  }
+
+
+function handle(event) {
+    const value = event.target.value;
+    console.log(value)
+    
+  }
 
 //Math function 
+let add = (a, b) => a + b 
+let substract = (a, b) => a - b 
+let multiply = (a, b) => a * b
+let divide = (a, b) => a / b
 
-function operate () {
-    add = (a, b) => a + b 
-    substract = (a, b) => a - b 
-    multiply = (a, b) => a * b
-    divide = (a, b) => a / b
-   
-}
+
 
 /* 
 Comment effectuer un calcul genre 10 + 20 ? 
 
 - Quand j'appuie une touche, affiche la touche a l'écran.
+    x Afficher la touche dans le console.log
+    - Afficher la touche à l'écran.
     - Lier ces touches aux inputs des functions
     
 - Quand je clique sur Equal montre le resultat. 
